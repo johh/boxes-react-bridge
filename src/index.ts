872d8bd493e498887af57555e8b991dd78fd9b5f
johh <code@johh.net>
2020-06-11 14:40:55 +0200

    feat: add UnifromProvider

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as BoxesRoot } from './lib/BoxesRoot';
 export { default as TransformNode } from './lib/TransformNode';
 export { default as Renderable } from './lib/Renderable';
+export { default as UniformProvide } from './lib/UniformProvider';
---
+++ src/lib/UniformProvider.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-param-reassign */
+import React, {
+	ForwardRefExoticComponent,
+	forwardRef,
+} from 'react';
+import { UniformProvider as _UniformProvider } from '@downpourdigital/boxes';
+import {
+	UniformProviderProps as _UniformProviderProps,
+} from '@downpourdigital/boxes/dist/lib/UniformProvider';
+
+import createInstance from './createInstance';
+import { applyTraversableProps, TraversableProps } from './Traversable';
+import TraversableChildren from './TraversableChildren';
+import mountNode from './mountNode';
+
+
+interface UniformProviderProps extends _UniformProviderProps, TraversableProps {}
+
+
+export function applyUniformProviderProps(
+	node: _UniformProvider,
+	props: UniformProviderProps,
+): void {
+	node.uniforms = props.uniforms;
+}
+
+const UniformProvider: ForwardRefExoticComponent<UniformProviderProps> = forwardRef( (
+	{ children, ...props }, ref,
+) => {
+	const uniformProvider = createInstance(
+		ref,
+		() => new _UniformProvider({
+			uniforms: props.uniforms,
+		}),
+	);
+
+	applyTraversableProps( uniformProvider, props );
+	applyUniformProviderProps( uniformProvider, props );
+	mountNode( uniformProvider );
+
+	return children && (
+		<TraversableChildren parent={uniformProvider}>
+			{children}
+		</TraversableChildren>
+	);
+});
+
+export default UniformProvider;
